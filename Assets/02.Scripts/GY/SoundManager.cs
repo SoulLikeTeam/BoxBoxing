32e2dc56a18a550ef13234ba668f4ea257aebd2a
kjp2981 <kjp29816265@gmail.com>
2022-12-03 22:38:04 +0900

    저번에 안올린 것들

--- Assets/02.Scripts/GY/SoundManager.cs
+++ Assets/02.Scripts/GY/SoundManager.cs
@@ -52,7 +52,7 @@ public class SoundManager : MonoBehaviour
 
     public void SFXPlay(string sfxName, AudioClip clip)
     {
-        Poolable go = Managers.Pool.Pop("Sound/Audio");
+        Poolable go = Managers.Resource.Instantiate("Sound/Audio").GetComponent<Poolable>();
         if(go.GetComponent<AudioSource>() == null)
             go.AddComponent<AudioSource>();
 
